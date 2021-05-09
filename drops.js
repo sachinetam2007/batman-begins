5ddf93b3dcb08e4dc7f9af5334ff701b094b8e8e
Sachi Netam <71205766+sachinetam2007@users.noreply.github.com>
2021-05-09 11:49:32 +0530

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,23 @@
+class Drops {
+    constructor(x,y){
+        var options={
+        friction:0.001,
+        restitution:0.1
+        }
+        this.rain=Bodies.circle(x,y,5,options)
+        World.add(world,this.rain)
+    }
+    display(){
+        fill("blue")
+        ellipseMode(CENTER)
+        ellipse (this.rain.position.x,this.rain.position.y,5,5)
+
+    }
+    
+    update(){
+        if(this.rain.position.y>height){
+            Matter.Body.setPosition(this.rain,{x:random (0,400),y:0})
+        }
+    }
+
+}
